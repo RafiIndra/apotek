0e9bc1ef204051c7854e13f9172ffd0e3a878165
RafiIndra <83796166+RafiIndra@users.noreply.github.com>
2022-12-02 10:36:33 +0700

    tes

--- application/views/tes/table_sup.php
+++ application/views/tes/table_sup.php
@@ -7,7 +7,7 @@
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
-                <h2>Lihat Pemasok</h2>
+                <h2>Lihat Pemasokk</h2>
                 <ul class="nav navbar-right panel_toolbox">
                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                     </li>
